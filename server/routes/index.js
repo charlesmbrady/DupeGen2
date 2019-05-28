62d000fb02dedde6dcb3dae9a7b023db45e05a12
Charles <charlesmbrady@gmail.com>
2019-05-28 15:30:35 -0400

    trying to get heroku deploy working

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
-const authRoutes = require("./auth");
 const apiRoutes = require("./api");
-
-// Auth Routes
-router.use('/auth', authRoutes);
 
 // API Routes
 router.use("/api", apiRoutes);
